aa5d03ef32bd67e876417be6356abb61866aaf5e
OSC55 <OSC5512@GMAIL.COM>
2016-05-24 23:01:20 -0700

    Add project files.

---
+++ ScoresList/Form1.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnList = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnFind = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtScore = New System.Windows.Forms.TextBox()
+        Me.txtScoreCount = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtScoreAverage = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtScoreMin = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtScoreMax = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(36, 170)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(89, 24)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "&Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(144, 168)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(85, 25)
+        Me.btnDelete.TabIndex = 1
+        Me.btnDelete.Text = "&Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnList
+        '
+        Me.btnList.Location = New System.Drawing.Point(258, 165)
+        Me.btnList.Name = "btnList"
+        Me.btnList.Size = New System.Drawing.Size(78, 28)
+        Me.btnList.TabIndex = 2
+        Me.btnList.Text = "&List"
+        Me.btnList.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(456, 165)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(90, 27)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnFind
+        '
+        Me.btnFind.Location = New System.Drawing.Point(356, 167)
+        Me.btnFind.Name = "btnFind"
+        Me.btnFind.Size = New System.Drawing.Size(76, 25)
+        Me.btnFind.TabIndex = 4
+        Me.btnFind.Text = "&Find"
+        Me.btnFind.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Score"
+        '
+        'txtScore
+        '
+        Me.txtScore.Location = New System.Drawing.Point(109, 16)
+        Me.txtScore.Name = "txtScore"
+        Me.txtScore.Size = New System.Drawing.Size(127, 20)
+        Me.txtScore.TabIndex = 6
+        '
+        'txtScoreCount
+        '
+        Me.txtScoreCount.Location = New System.Drawing.Point(109, 42)
+        Me.txtScoreCount.Name = "txtScoreCount"
+        Me.txtScoreCount.ReadOnly = True
+        Me.txtScoreCount.Size = New System.Drawing.Size(127, 20)
+        Me.txtScoreCount.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 47)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Count"
+        '
+        'txtScoreAverage
+        '
+        Me.txtScoreAverage.Location = New System.Drawing.Point(109, 68)
+        Me.txtScoreAverage.Name = "txtScoreAverage"
+        Me.txtScoreAverage.ReadOnly = True
+        Me.txtScoreAverage.Size = New System.Drawing.Size(127, 20)
+        Me.txtScoreAverage.TabIndex = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 73)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Average"
+        '
+        'txtScoreMin
+        '
+        Me.txtScoreMin.Location = New System.Drawing.Point(109, 94)
+        Me.txtScoreMin.Name = "txtScoreMin"
+        Me.txtScoreMin.ReadOnly = True
+        Me.txtScoreMin.Size = New System.Drawing.Size(127, 20)
+        Me.txtScoreMin.TabIndex = 12
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(33, 99)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(24, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Min"
+        '
+        'txtScoreMax
+        '
+        Me.txtScoreMax.Location = New System.Drawing.Point(109, 120)
+        Me.txtScoreMax.Name = "txtScoreMax"
+        Me.txtScoreMax.ReadOnly = True
+        Me.txtScoreMax.Size = New System.Drawing.Size(127, 20)
+        Me.txtScoreMax.TabIndex = 14
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(33, 125)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(27, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Max"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(618, 229)
+        Me.Controls.Add(Me.txtScoreMax)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtScoreMin)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtScoreAverage)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtScoreCount)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtScore)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnFind)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnList)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnList As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnFind As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtScore As System.Windows.Forms.TextBox
+    Friend WithEvents txtScoreCount As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtScoreAverage As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtScoreMin As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtScoreMax As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ ScoresList/Form1.vb
@@ -0,0 +1,206 @@
+﻿Public Class Form1
+
+    'Class level variables
+    Dim scoreList As List(Of Integer)
+    Dim scoreCount As Integer
+    Dim scoreMax As Decimal
+    Dim scoreMin As Decimal
+    Dim scoreAverage As Decimal
+    Dim strError As String
+
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        'reset all errors
+        strError = Nothing
+
+        'validate all form
+        validateForm()
+
+        If String.IsNullOrEmpty(strError) Then
+            'No errors go ahead and add the score to the list
+            scoreList.Add(txtScore.Text)
+
+            'Increment count class variable
+            scoreCount += 1
+            txtScoreCount.text = scoreCount
+
+            'display averageScore
+            txtScoreAverage.Text = getScoreAverage()
+
+            'display minscore
+            txtScoreMax.Text = getScoreMax()
+
+            'display average score
+            txtScoreMin.Text = getScoreMin()
+        Else
+            'there was an error. Display it to the user
+            MessageBox.Show(strError)
+        End If
+    End Sub
+
+    Private Function getScoreAverage() As Decimal
+        'Calculate average
+
+        Dim decTotal As Decimal = 0
+        Dim decAverage As Decimal = 0
+
+        For i As Integer = 0 To scoreList.Count - 1
+            decTotal += scoreList(i)
+        Next
+
+        decAverage = decTotal / scoreCount
+
+        Return decAverage
+
+    End Function
+
+    Private Function getScoremax() As Integer
+        'Calculate Max
+
+        Dim intMax As Integer = scoreList(0)
+
+        For i As Integer = 0 To scoreList.Count - 1
+            If scoreList(i) > intMax Then
+                intMax = scoreList(i)
+            End If
+        Next
+
+        Return intMax
+    End Function
+
+    Private Function getScoreMin() As Integer
+        'Calculate Min
+
+        Dim intMin As Integer = scoreList(0)
+
+        For i As Integer = 0 To scoreList.Count - 1
+            If scoreList(i) < intMin Then
+                intMin = scoreList(i)
+            End If
+        Next
+
+        Return intMin
+    End Function
+
+    Private Sub validateForm()
+        'validate individual controls
+        getValidScore(txtScore.Text)
+    End Sub
+
+    Private Sub getValidScore(input As String)
+        Dim aScore As Integer
+        Try
+            'Make sure it's an Int
+            aScore = Convert.ToInt32(input)
+
+            'Make sure it's positive
+            If (aScore <= 0) Then
+                addError("Invalid score entered. Scores should be greater than 0")
+            End If
+
+        Catch ex As Exception
+            'Not an int
+            addError("Invalid score entered. Scores should be integers")
+        End Try
+    End Sub
+
+    Private Sub addError(anError As String)
+        'add errors to the list of errors
+        If String.IsNullOrEmpty(strError) Then
+            strError = "There was an error processing your request" & vbCrLf & vbCrLf
+            strError += anError & vbCrLf
+        Else
+            strError += anError & vbCrLf
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        scoreList = New List(Of Integer)
+    End Sub
+
+    Private Sub btnList_Click(sender As Object, e As EventArgs) Handles btnList.Click
+        'List Scores
+
+        If scoreList.Count > 0 Then
+            Dim strSummary = "Score list" & vbCrLf & vbCrLf
+
+            For i As Integer = 0 To scoreList.Count - 1
+                strSummary += scoreList(i) & vbCrLf
+            Next
+
+            MessageBox.Show(strSummary)
+
+        Else
+            MessageBox.Show("List of scores is empty. Please add scores")
+        End If
+
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim aScore As Integer = InputBox("Enter score")
+
+        If scoreList.Count = 0 Then
+            MessageBox.Show("List of scores is empty. Please add scores")
+            Exit Sub
+        End If
+        For i As Integer = 0 To scoreList.Count
+            If aScore = scoreList(i) Then
+                'Found a matching score
+                'Need to remove from list
+
+                scoreList.Remove(aScore)
+
+
+                'clear the score
+                txtScore.Text = ""
+
+                'Increment count class variable
+                scoreCount -= 1
+                txtScoreCount.Text = scoreCount
+
+                'display averageScore
+                txtScoreAverage.Text = getScoreAverage()
+
+                'display minscore
+                txtScoreMax.Text = getScoremax()
+
+                'display average score
+                txtScoreMin.Text = getScoreMin()
+
+                MessageBox.Show("Removed score " & aScore & " at index " & i)
+                Exit For
+            End If
+        Next
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear List
+        scoreList.Clear()
+
+        'Clear form
+        For Each ctrl In Me.Controls
+            If TypeOf (ctrl) Is TextBox Then
+                ctrl.text = ""
+            End If
+        Next
+
+    End Sub
+
+    Private Sub btnFind_Click(sender As Object, e As EventArgs) Handles btnFind.Click
+        Dim aScore As Integer = InputBox("Enter score")
+
+        If scoreList.Count = 0 Then
+            MessageBox.Show("List of scores is empty. Please add scores")
+            Exit Sub
+        End If
+        For i As Integer = 0 To scoreList.Count
+            If aScore = scoreList(i) Then
+                'Found  matching score
+
+                MessageBox.Show("Found score " & aScore & " at index " & i)
+                Exit For
+            End If
+        Next
+    End Sub
+End Class
